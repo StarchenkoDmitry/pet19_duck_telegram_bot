14fff5581b6e1cca377ad1308dd5648c3a37ebbe
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-05-05 22:54:50 +0300

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,89 @@
 import 'dotenv/config'
+import { IsNullOrUndefined, IsString } from './utils/check';
 
+export class Config{
 
-export function getToken():string {
-    const token = process.env.TELEGRAM_TOKEN;
-
-    if(token === null || token === undefined){
-        throw new Error("TELEGRAM_TOKEN is undefined.")
+    static get TELEGRAM_TOKEN():string {
+        const data = process.env.TELEGRAM_TOKEN;
+    
+        if(data === null || data === undefined){
+            throw new Error("TELEGRAM_TOKEN is undefined.");
+        }
+    
+        if(!IsString(data)){
+            throw new Error("TELEGRAM_TOKEN is not string type.");
+        }
+    
+        return data;
     }
 
-    if(typeof token !== "string"){
-        throw new Error("TELEGRAM_TOKEN is not string type.")
+    static get DB_HOST():string {
+        const data = process.env.DB_HOST;
+
+        if(data === null || data === undefined){
+            throw new Error("DB_HOST is undefined.");
+        }
+    
+        if(!IsString(data)){
+            throw new Error("DB_HOST is not string type.");
+        }
+    
+        return data;
+    }
+    
+    static get DB_PORT():number {
+        const data = process.env.DB_PORT;
+
+        if(data === null || data === undefined){
+            throw new Error("DB_PORT is undefined.");
+        }
+    
+        if(!IsString(data)){
+            throw new Error("DB_PORT is not string type.");
+        }
+    
+        return parseInt(data);
     }
 
-    return token;
+    static get DB_DATABASE():string {
+        const data = process.env.DB_DATABASE;
+
+        if(data === null || data === undefined){
+            throw new Error("DB_DATABASE is undefined.");
+        }
+    
+        if(!IsString(data)){
+            throw new Error("DB_DATABASE is not string type.");
+        }
+    
+        return data;
+    }
+    
+    static get DB_USER():string {
+        const data = process.env.DB_USER;
+
+        if(data === null || data === undefined){
+            throw new Error("DB_USER is undefined.");
+        }
+    
+        if(!IsString(data)){
+            throw new Error("DB_USER is not string type.");
+        }
+    
+        return data;
+    }
+    
+    static get DB_PASSWORD():string {
+        const data = process.env.DB_PASSWORD;
+
+        if(data === null || data === undefined){
+            throw new Error("DB_PASSWORD is undefined.");
+        }
+    
+        if(!IsString(data)){
+            throw new Error("DB_PASSWORD is not string type.");
+        }
+    
+        return data;
+    }
 }
